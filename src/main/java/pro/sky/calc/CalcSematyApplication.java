7854123c74ae884472166c55a7ea2d24074bd5cf
tychk <semarooney@yandex.ru>
2024-07-27 11:56:36 +0300

    HW_SpringBoot is Ready

---
+++ src/main/java/pro/sky/calc/CalcSematyApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcSematyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcSematyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/calc/CalcSematyApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcSematyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
