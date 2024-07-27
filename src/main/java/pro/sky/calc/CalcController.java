a5da3c00b7c4ba91bbd5e36ba780ef42ac80f5ef
tychk <semarooney@yandex.ru>
2024-07-27 13:30:20 +0300

    HW_SpringBoot is Ready with check param

--- src/main/java/pro/sky/calc/CalcController.java
+++ src/main/java/pro/sky/calc/CalcController.java
@@ -12,29 +12,51 @@ public class CalcController {
     public CalcController(CalcService calcService) {
         this.calcService = calcService;
     }
+
     @GetMapping(path = "/calculator")
     public String hello() {
         return calcService.helloCalc();
     }
 
     @GetMapping(path = "/calculator/plus")
-    public String calcPlus(@RequestParam("num1") int numb1, @RequestParam("num2") int numb2) {
-        return calcService.calcPlus(numb1,numb2);
+    public String calcPlus(@RequestParam(value = "num1", required = false) Integer numb1,
+                           @RequestParam(value = "num2", required = false) Integer numb2) {
+        if (numb1 == null || numb2 == null) {
+            return "Не передан параметр";
+        } else {
+            return calcService.calcPlus(numb1, numb2);
+        }
     }
 
     @GetMapping(path = "/calculator/minus")
-    public String calcMinus(@RequestParam("num1") int numb1, @RequestParam("num2") int numb2) {
-        return calcService.calcMinus(numb1,numb2);
+    public String calcMinus(@RequestParam(value = "num1", required = false) Integer numb1,
+                            @RequestParam(value = "num2", required = false) Integer numb2) {
+        if (numb1 == null || numb2 == null) {
+            return "Не передан параметр";
+        } else {
+            return calcService.calcMinus(numb1, numb2);
+        }
     }
 
     @GetMapping(path = "/calculator/multiply")
-    public String calcMultiply(@RequestParam("num1") int numb1, @RequestParam("num2") int numb2) {
-        return calcService.calcMultiply(numb1,numb2);
+    public String calcMultiply(@RequestParam(value = "num1", required = false) Integer numb1,
+                               @RequestParam(value = "num2", required = false) Integer numb2) {
+        if (numb1 == null || numb2 == null) {
+            return "Не передан параметр";
+        } else {
+            return calcService.calcMultiply(numb1, numb2);
+        }
     }
 
     @GetMapping(path = "/calculator/divide")
-    public String calcDivide(@RequestParam("num1") int numb1, @RequestParam("num2") int numb2) {
-        return calcService.calcDivide(numb1, numb2);
+    public String calcDivide(@RequestParam(value = "num1", required = false) Integer numb1,
+                             @RequestParam(value = "num2", required = false) Integer numb2) {
+        if (numb1 == null || numb2 == null) {
+            return "Не передан параметр";
+        } else {
+            return calcService.calcDivide(numb1, numb2);
+        }
     }
-
 }
+
+
